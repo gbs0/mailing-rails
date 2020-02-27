250f427bfc211651c6c44865787076871eca8c5c
gabriel schiavo <gabriel@lewagon.org>
2020-02-27 12:15:18 -0300

    add method for after user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_create :subscribe_to_newsletter
+
+  private
+
+  def subscribe_to_newsletter
+  	SubscribeToNewsletterService.new(self).call
+  end
 end
